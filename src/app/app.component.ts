e8f0ce7227c564c8f12d27864f6c61504add9cf7
Daniil Dubrava <daniil.dubrava@yandex.ru>
2023-11-04 22:12:10 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
 
+/**
+ * The best component.
+ *
+ * @example
+ * ```html
+ * <ya-map
+ *   [center]="[55.751952, 37.600739]"
+ *   [state]="{type: 'yandex#satellite'}"
+ * ></ya-map>
+ * ```
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
